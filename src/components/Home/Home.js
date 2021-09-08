96c0f3c8674567f22ffc3f682644025b8eddc5c7
Umer Aftab <umeraftab7747@gmail.com>
2021-09-09 00:51:08 +0500

    add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,19 @@
 // rfce
 import React from "react";
+import "./Home.scss";
+import img from "../../assets/lucrezia-carnelos-wQ9VuP_Njr4-unsplash.jpg";
 
 function Home() {
   return (
-    <div>
-      <h1>HOME</h1>
+    <div className="MainContainer">
+      <div className="BgImage">
+        <div className="screenoverlay">
+          <div className="txtcontainer">
+            <p className="HeaderText">Welcome to Ecom-Shop</p>
+            <p className="SloganText">"We’re in Business to Improve Lives"</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
